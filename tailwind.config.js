efbdfbf53060073ca4ac868170d319126403751a
present-guernina <antilope.16.tuinsteden@icloud.com>
2025-06-26 16:31:16 +0200

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,1 +1,16 @@
-
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#003328",
+        "primary-dark": "#00271f",
+        accent: "#000000",
+        light: "#F9F9F9",
+        gold: "#FFD700"
+      },
+      fontFamily: { base: ["Aptos", "Arial", "Helvetica", "sans-serif"] }
+    }
+  },
+  plugins: []
+};
